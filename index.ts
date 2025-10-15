e28e5be2ef67c5489949b6edc079994f8a496f09
Hugof01 <97979072+Hugof01@users.noreply.github.com>
2025-10-15 09:33:21 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -17,7 +17,7 @@ async function handler(_req: Request): Promise<Response> {
   headers.append("Content-Type", "application/json");
 
   const similarityRequestBody = JSON.stringify({
-    word1: "centrale",
+    word1: "supelec",
     word2: "supelec",
   });
 
